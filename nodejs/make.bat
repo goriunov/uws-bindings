d63723e041c4e3e8c00addb73665744c416c53a0
Dmitrii Goriunov <goriunovd@gmail.com>
2018-03-28 10:50:51 +1300

    Update bindings code

--- nodejs/make.bat
+++ nodejs/make.bat
@@ -18,7 +18,6 @@ cp -r ../uWebSockets/src dist/
 cp src/addon.cpp dist/src/addon.cpp
 cp src/addon.h dist/src/addon.h
 cp src/http.h dist/src/http.h
-cp src/uws.js dist/uws.js
 
 cl /I targets/node-v6.13.1/include/node /EHsc /Ox /LD /Fedist/uws_win32_48.node dist/src/*.cpp targets/node-v6.13.1/node.lib
 cl /I targets/node-v7.10.1/include/node /EHsc /Ox /LD /Fedist/uws_win32_51.node dist/src/*.cpp targets/node-v7.10.1/node.lib
