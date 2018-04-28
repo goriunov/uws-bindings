5f1a95eb2fc199fc2ec5ac111035435a4860847b
Dmitrii Goriunov <goriunovd@gmail.com>
2018-04-28 12:56:45 +1200

    Drop version of node

--- nodejs/make.bat
+++ nodejs/make.bat
@@ -2,10 +2,6 @@ call "%VS140COMNTOOLS%..\..\vc\vcvarsall.bat" amd64
 
 if not exist targets (
 mkdir targets
-curl https://nodejs.org/dist/v6.14.1/node-v6.14.1-headers.tar.gz | tar xz -C targets
-curl https://nodejs.org/dist/v6.14.1/win-x64/node.lib > targets/node-v6.14.1/node.lib
-curl https://nodejs.org/dist/v7.10.1/node-v7.10.1-headers.tar.gz | tar xz -C targets
-curl https://nodejs.org/dist/v7.10.1/win-x64/node.lib > targets/node-v7.10.1/node.lib
 curl https://nodejs.org/dist/v8.11.1/node-v8.11.1-headers.tar.gz | tar xz -C targets
 curl https://nodejs.org/dist/v8.11.1/win-x64/node.lib > targets/node-v8.11.1/node.lib
 curl https://nodejs.org/dist/v9.10.1/node-v9.10.1-headers.tar.gz | tar xz -C targets
@@ -21,8 +17,6 @@ cp src/addon.cpp dist/src/addon.cpp
 cp src/addon.h dist/src/addon.h
 cp src/http.h dist/src/http.h
 
-cl /I targets/node-v6.14.1/include/node /EHsc /Ox /LD /Fedist/uws_win32_48.node dist/src/*.cpp targets/node-v6.14.1/node.lib
-cl /I targets/node-v7.10.1/include/node /EHsc /Ox /LD /Fedist/uws_win32_51.node dist/src/*.cpp targets/node-v7.10.1/node.lib
 cl /I targets/node-v8.11.1/include/node /EHsc /Ox /LD /Fedist/uws_win32_57.node dist/src/*.cpp targets/node-v8.11.1/node.lib
 cl /I targets/node-v9.10.1/include/node /EHsc /Ox /LD /Fedist/uws_win32_59.node dist/src/*.cpp targets/node-v9.10.1/node.lib
 cl /I targets/node-v10.0.0/include/node /EHsc /Ox /LD /Fedist/uws_win32_64.node dist/src/*.cpp targets/node-v10.0.0/node.lib
